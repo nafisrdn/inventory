57ce37025344ea28092c3386fb1d9bb3dfa765ac
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-16 13:45:36 +0700

    Setup DB

---
+++ src/configs/db.config.js
@@ -0,0 +1,5 @@
+module.exports.HOST = process.env.DB_HOST;
+module.exports.PORT = process.env.DB_PORT;
+module.exports.USER = process.env.DB_USER;
+module.exports.PASSWORD = process.env.DB_PASSWORD;
+module.exports.DATABASE = process.env.DB_DATABASE;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
+require("dotenv").config();
+
 const path = require("path");
+const { Sequelize, DataTypes } = require("sequelize");
+const dbConfig = require("./configs/db.config");
 
 const { convertTextFilesToJson } = require("./utils/json-converter.util");
 
-const directoryPath = path.join(__dirname, "../input");
+const inputDirectoryPath = path.join(__dirname, "../input");
+
+const setupOrm = async () => {
+  const sequelize = new Sequelize(
+    dbConfig.DATABASE,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+      host: dbConfig.HOST,
+      dialect: "mysql",
+      port: dbConfig.PORT,
+    }
+  );
+
+  await sequelize.authenticate();
+  console.log("DB Connected");
+
+  return sequelize;
+};
 
 (async () => {
-  const data = await convertTextFilesToJson(directoryPath);
-  console.log(data);
+  try {
+    const data = await convertTextFilesToJson(inputDirectoryPath);
+
+    const sequelize = await setupOrm();
+
+    connection.end();
+  } catch (error) {
+    console.error(error);
+  }
 })();
