b3ab6a0f5f6c9bc43c594b2681eec9fe26dfd945
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-16 14:27:31 +0700

    Add insert os info data to db

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const path = require("path");
 
 const { sequelize } = require("./configs/db.config");
-const { createOsInfo, recreateTable } = require("./services/os-info.service");
+const {
+  createOsInfo,
+  recreateTable,
+  bulkCreate,
+} = require("./services/os-info.service");
 const { convertTextFilesToJson } = require("./utils/json-converter.util");
 
 const inputDirectoryPath = path.join(__dirname, "../input");
@@ -14,25 +18,15 @@ const authenticateDb = async () => {
 
 (async () => {
   try {
-    const data = await convertTextFilesToJson(inputDirectoryPath);
+    const osInfos = await convertTextFilesToJson(inputDirectoryPath);
 
     await authenticateDb();
 
     await recreateTable();
 
-    const osInfo = await createOsInfo({
-      outlet_code: 765765,
-      username: "root",
-      hostname: "jenkins-ubuntu",
-      ip_address: "10.2.0.4",
-      ram_gb: 7,
-      cpu: "Intel(R) Xeon(R) Platinum",
-      disk: "29G",
-      os_version: "Ubuntu 20.04.5 LTS",
-      java_version: "11.0.17",
-      jdk_version: "11.0.17",
-      serial_number: "0000-0007-8585-3300-8170-6992-42",
-    });
+    const insertedOsInfos = await bulkCreate(osInfos);
+
+    console.log(insertedOsInfos);
 
     await sequelize.close();
   } catch (error) {
--- src/services/os-info.service.js
+++ src/services/os-info.service.js
@@ -13,5 +13,12 @@ const recreateTable = async () => {
   console.log("Table recreated successfully!");
 };
 
+const bulkCreate = async (infos) => {
+  const insertedOsInfo = await OsInfo.bulkCreate(infos);
+
+  return insertedOsInfo;
+};
+
 module.exports.createOsInfo = createOsInfo;
 module.exports.recreateTable = recreateTable;
+module.exports.bulkCreate = bulkCreate;
